f97dd9afe35c0ddcde98336c81a8cf90c77ea6b4
Jane Pavlova <35457637+pavlovajane@users.noreply.github.com>
2023-06-30 15:03:04 -0400

    Add basic auth logic

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -1,11 +1,13 @@
 from typing import List
+from werkzeug.security import check_password_hash
+from swagger_server.services.user_service import get_password_hash
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
 def check_basicAuth(username, password, required_scopes):
-    if username == "test" and password == "test":
-        return {'test_key': 'test_value'}
+    if check_password_hash(get_password_hash(username), password):
+        return {"sub": username}
     return None
 
 
--- swagger_server/services/user_service.py
+++ swagger_server/services/user_service.py
@@ -1,4 +1,5 @@
 from injector import inject
+from swagger_server.models.tops import Tops
 from swagger_server.models.user import User
 from swagger_server.models.users_body import UsersBody
 from swagger_server.persistence.repository import DbRepository
@@ -14,11 +15,29 @@ class UserService:
     def create_user(self, user: UsersBody) -> User:
         username = user.username
         hash = generate_password_hash(user.password)
-        q = """
+        query = """
             INSERT INTO users (username, hash) 
                        VALUES (?, ?)
         """
-        self.repo.execute_and_commit(q, (username, hash,))
+        self.repo.execute_and_commit(query, (username, hash,))
         rows = self.repo.find_all("SELECT id FROM users WHERE username = ?", (username,))
+        if len(rows) == 0:
+            return None
         id, = rows[0]
         return User(id=id)
+    
+    def get_user_tops(self, repo: DbRepository) -> Tops:
+        pass
+
+    def get_password_hash(self, username: str) -> str:
+
+        query = """
+                SELECT 
+                hash 
+                FROM users WHERE username = ?"""
+        rows = self.repo.find_all(query, (username,))
+        if len(rows) == 0:
+            return None
+        hash = rows[0]
+        return hash
+        
